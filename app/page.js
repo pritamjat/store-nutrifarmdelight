83b8258863db2a811d981b0fce62984039c9ed36
pritamjat <116189257+pritamjat@users.noreply.github.com>
2026-02-10 22:32:24 +0530

    Remove product listing and add ProductList component

--- app/page.js
+++ app/page.js
@@ -1,19 +1,10 @@
-import Link from "next/link";
 import { cookies } from "next/headers";
 import { verifyToken } from "@/lib/auth";
+import ProductList from "@/app/components/ProductList";
 
 export default async function HomePage() {
   const cookieStore = cookies();
   const token = cookieStore.get("auth_token")?.value;
-
-  async function handleAdd(product) {
-  await fetch("/api/cart/add", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ product }),
-  });
-}
-
 
   let user = null;
 
@@ -25,8 +16,6 @@ export default async function HomePage() {
     }
   }
 
-  const isLoggedIn = !!user;
-
   return (
     <div>
 
@@ -35,28 +24,8 @@ export default async function HomePage() {
         <p>Flat 30% OFF on first order</p>
       </section>
 
-      <section className="products">
-        <h2>Trending Products</h2>
-        <div className="grid">
-          {[1,2,3,4].map((item) => (
-            <div key={item} className="product-card">
-              <div className="image-placeholder"></div>
-              <h3>Organic Product {item}</h3>
-              <p>₹299</p>
-              <button onClick={()=>
-                  handleAdd({
-                   productId:item,
-                   name:`Organic Product${item}`,
-                   price: 299})
-                 }
-                 >Add to Cart</button>
-            </div>
-          ))}
-        </div>
-      </section>
+      <ProductList />
 
     </div>
-
   );
 }
-
