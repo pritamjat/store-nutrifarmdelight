b1983fe76974f0c7ffef3caafb9944aa6acb4a49
pritamjat <116189257+pritamjat@users.noreply.github.com>
2026-02-11 16:38:22 +0530

    Update CartIcon.js

--- app/components/CartIcon.js
+++ app/components/CartIcon.js
@@ -1,1 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { useCart } from "@/app/context/CartContext";
+
+export default function CartIcon() {
+  const { cartCount } = useCart();
+
+  return (
+    <Link href="/cart" style={{ position: "relative" }}>
+      🛒
+      {cartCount > 0 && (
+        <span
+          style={{
+            position: "absolute",
+            top: "-8px",
+            right: "-10px",
+            background: "red",
+            color: "white",
+            borderRadius: "50%",
+            padding: "3px 7px",
+            fontSize: "12px",
+          }}
+        >
+          {cartCount}
+        </span>
+      )}
+    </Link>
+  );
+}
 
