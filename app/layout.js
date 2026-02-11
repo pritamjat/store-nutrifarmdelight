bc5278e8b4c6504b533254d0815a7442e1b0ea0f
pritamjat <116189257+pritamjat@users.noreply.github.com>
2026-02-11 15:44:48 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,28 +13,9 @@ export default async function RootLayout({ children }) {
   const token = cookieStore.get("auth_token")?.value;
 
   let user = null;
-  
   let cartCount = 0;
 
-if (user) {
-  const client = await clientPromise;
-  const db = client.db();
-  const users = db.collection("users");
-
-  const dbUser = await users.findOne(
-    { _id: new ObjectId(user.sub) },
-    { projection: { cart: 1 } }
-  );
-
-  if (dbUser?.cart) {
-    cartCount = dbUser.cart.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
-  }
-}
-
-
+  // ✅ First verify user
   if (token) {
     try {
       user = await verifyToken(token);
@@ -43,13 +24,30 @@ if (user) {
     }
   }
 
+  // ✅ Then fetch cart if user exists
+  if (user) {
+    const client = await clientPromise;
+    const db = client.db();
+    const users = db.collection("users");
+
+    const dbUser = await users.findOne(
+      { _id: new ObjectId(user.sub) },
+      { projection: { cart: 1 } }
+    );
+
+    if (dbUser?.cart) {
+      cartCount = dbUser.cart.reduce(
+        (sum, item) => sum + item.quantity,
+        0
+      );
+    }
+  }
+
   const isLoggedIn = !!user;
 
   return (
     <html lang="en">
       <body>
-
-        {/* ===== GLOBAL NAVBAR ===== */}
         <header className="navbar">
           <div className="logo">
             <Link href="/">NutriFarm</Link>
@@ -69,35 +67,35 @@ if (user) {
               </>
             ) : (
               <>
-               <Link href="/cart" style={{ position: "relative" }}>
-                         🛒
-               {cartCount > 0 && (
-               <span
-               style={{
-                position: "absolute",
-                top: "-8px",
-               right: "-10px",
-               background: "red",
-               color: "white",
-               borderRadius: "50%",
-              padding: "3px 7px",
-             fontSize: "12px",
-           }}
-           >
-             {cartCount}
-              </span>
-       )}
-       </Link>
+                <ProfileDropdown name={user.name} />
 
+                <Link href="/cart" style={{ position: "relative" }}>
+                  🛒
+                  {cartCount > 0 && (
+                    <span
+                      style={{
+                        position: "absolute",
+                        top: "-8px",
+                        right: "-10px",
+                        background: "red",
+                        color: "white",
+                        borderRadius: "50%",
+                        padding: "3px 7px",
+                        fontSize: "12px",
+                      }}
+                    >
+                      {cartCount}
+                    </span>
+                  )}
+                </Link>
               </>
             )}
           </div>
         </header>
 
-        {/* Page Content */}
         {children}
-
       </body>
     </html>
   );
 }
+
