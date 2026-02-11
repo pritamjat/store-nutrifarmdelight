f80dafdb6c2ea73c1814523d798ec2d14a18a930
pritamjat <116189257+pritamjat@users.noreply.github.com>
2026-02-11 13:39:27 +0530

    Implement AddToCartButton component

--- app/product/[id]/AddToCartButton.js
+++ app/product/[id]/AddToCartButton.js
@@ -1,1 +1,23 @@
+"use client";
+
+export default function AddToCartButton({ product }) {
+
+  async function handleAdd() {
+    await fetch("/api/cart/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product }),
+    });
+
+    alert("Added to cart!");
+  }
+
+  return (
+    <button onClick={handleAdd}>
+      Add to Cart
+    </button>
+  );
+}
 
