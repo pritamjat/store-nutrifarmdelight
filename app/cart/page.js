8d038cacc8c8907287665f5f9abf5bd24afb5d60
pritamjat <116189257+pritamjat@users.noreply.github.com>
2026-02-12 15:58:27 +0530

    Update page.js

--- app/cart/page.js
+++ app/cart/page.js
@@ -97,6 +97,12 @@ async function handleCheckout() {
 }
 
 
+  useEffect(() => {
+  const script = document.createElement("script");
+  script.src = "https://checkout.razorpay.com/v1/checkout.js";
+  script.async = true;
+  document.body.appendChild(script);
+}, []);
 
 
 
